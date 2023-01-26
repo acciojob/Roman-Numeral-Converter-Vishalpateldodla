f9a0de7da74536f9ca2ab319c414e388ae5d2f87
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-01-26 14:37:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,31 @@ function convertToRoman(num) {
     };
 
   //your code here
+	function toRoman(num) {
+    var roman = "";
+    var romanNumerals = [        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
+    for (var i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i][0]) {
+            roman += romanNumerals[i][1];
+            num -= romanNumerals[i][0];
+        }
+    }
+    return roman.toUpperCase();
+}
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
